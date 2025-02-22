7695e9f52e83a301a159c3c3b8c7d9a641797107
Sebastian <sebastianabraham2006@gmail.com>
2025-02-22 10:27:40 +0530

    feat: login functionality

--- api/controller/UserFunctions.go
+++ api/controller/UserFunctions.go
@@ -52,9 +52,9 @@ func VerifyUser(db *gorm.DB) func(*fiber.Ctx) error {
 
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, errors.New("invalid token")
+				return nil, errors.New("invalid token Here")
 			}
-			return []byte(os.Getenv("JWT_SECRET")), nil
+			return []byte(os.Getenv("SECRET_KEY")), nil
 		})
 
 		if err != nil || !token.Valid {
@@ -135,6 +135,61 @@ func CreateUser(db *gorm.DB) func(*fiber.Ctx) error {
 		return c.Status(201).JSON(fiber.Map{
 			"message": "User created",
 			"data":    user,
+			"token":   token,
+		})
+	}
+}
+
+func Login(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		userData := new(models.User)
+
+		err := c.BodyParser(userData)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Could not parse Body",
+				"error":   err.Error(),
+			})
+		}
+
+		if userData.Email == "" || userData.Password == "" {
+			return c.Status(401).JSON(fiber.Map{
+				"message": "Email or Password is missing",
+			})
+		}
+
+		user := new(models.User)
+		err = db.Where("email = ?", userData.Email).First(user).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "record not found") {
+				return c.Status(404).JSON(fiber.Map{
+					"message": "User not found",
+				})
+			}
+
+			return c.Status(500).JSON(fiber.Map{
+				"message": "Could not retrieve user",
+				"error":   err.Error(),
+			})
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userData.Password))
+		if err != nil {
+			return c.Status(402).JSON(fiber.Map{
+				"message": "Invalid Password",
+			})
+		}
+
+		token, err := GenerateJWT(user)
+		if err != nil {
+			return c.Status(502).JSON(fiber.Map{
+				"message": "Could not generate token",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"message": "User logged in",
 			"token":   token,
 		})
 	}
