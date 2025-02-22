ff1288b770618bf3165dbc2574915410c97166c7
Hari Narayan <harinarayanmr@aol.com>
2025-02-22 12:25:37 +0530

    added routes for login and signup

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    let username = $state("");
+    let password = $state("");
+    const onclick = () => {
+        alert("button pressed");
+    };
+</script>
+
+<main>
+    <h1>login</h1>
+    <input type="text" bind:value={username} />
+    <input type="password" bind:value={password} />
+    <button {onclick}>login</button>
+</main>
+
+<style>
+</style>
