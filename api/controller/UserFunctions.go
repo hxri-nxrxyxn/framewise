290375fed790c304896e740ea1506e8b0721ab60
Sebastian <sebastianabraham2006@gmail.com>
2025-02-22 10:15:44 +0530

    feat: added create user functionality

--- api/controller/UserFunctions.go
+++ api/controller/UserFunctions.go
@@ -3,11 +3,15 @@ package controller
 import (
 	"errors"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/framewise/models"
+	"github.com/gofiber/fiber"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func GenerateJWT(user *models.User) (string, error) {
@@ -33,4 +37,63 @@ func GenerateJWT(user *models.User) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func CreateUser(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		user := new(models.User)
+
+		err := c.BodyParser(user)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Could not parse Body",
+				"error":   err.Error(),
+			})
+		}
+
+		if user.Email == "" || user.Password == "" {
+			return c.Status(401).JSON(fiber.Map{
+				"message": "Email or Password is missing",
+			})
+		}
+
+		hasedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"message": "Could not hash password",
+				"error":   err.Error(),
+			})
+		}
+
+		user.Password = string(hasedPassword)
+
+		err = db.Create(user).Error
+		if err != nil {
+
+			if strings.Contains(err.Error(), "SQLSTATE 23505") {
+				return c.Status(402).JSON(fiber.Map{
+					"message": "Email already exists",
+				})
+			}
+
+			return c.Status(501).JSON(fiber.Map{
+				"message": "Could not create user",
+				"error":   err.Error(),
+			})
+		}
+
+		token, err := GenerateJWT(user)
+		if err != nil {
+			return c.Status(502).JSON(fiber.Map{
+				"message": "Could not generate token",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(201).JSON(fiber.Map{
+			"message": "User created",
+			"data":    user,
+			"token":   token,
+		})
+	}
 }
