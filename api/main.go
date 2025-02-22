0e62c803337f0dbf31e76c40115140b5e147b8be
Sebastian <sebastianabraham2006@gmail.com>
2025-02-22 09:57:56 +0530

    intilized basic server structure

---
+++ api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "*",
+		AllowHeaders: "Content-Type,Authorization",
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":8080")
+}
