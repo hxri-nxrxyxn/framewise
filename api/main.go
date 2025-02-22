ff89d8185ebc3ce9c2de7117a46486794295fb3b
Sebastian <sebastianabraham2006@gmail.com>
2025-02-22 10:06:12 +0530

    imlemented database connectivity and user model migration to database

--- api/main.go
+++ api/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+
+	"github.com/framewise/database"
+	"github.com/framewise/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -14,9 +18,16 @@ func main() {
 		AllowHeaders: "Content-Type,Authorization",
 	}))
 
+	db, err := database.NewConnection()
+	if err != nil {
+		log.Fatal("Could not load database")
+	}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	models.MigrateUser(db)
 
 	app.Listen(":8080")
 }
